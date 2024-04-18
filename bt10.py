d1c8556e78fc753cf9df3cb9900ddc53fcb2dafb
quy17a2 <149318225+quy17a2@users.noreply.github.com>
2024-04-18 23:02:56 +0700

    Delete bt10.py

--- bt10.py
+++
@@ -1,7 +0,0 @@
-
-chuoi = input("Nhập chuỗi: ")
-result_string = ""
-for char in chuoi:
-    if char != ' ':
-        result_string += char
-print("Chuỗi sau khi loại bỏ khoảng trắng:", result_string)
